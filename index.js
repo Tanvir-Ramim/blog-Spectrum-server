4bc58a0c5b930a1574560762a876502bfdb83b54
Tanvir Ramim <ramimhossan14@gmail.com>
2023-11-06 03:21:54 +0600

    details api add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const cors=require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app=express()
 const port=process.env.PORT || 5000
@@ -47,11 +47,24 @@ async function run() {
        app.get('/recentBlog',async(req,res)=>{
           try{
             const result= await blogCollection.find().sort({currentTime:-1}).limit(6) .toArray()
-            res.send(result)
+            return res.send(result)
           }
           catch{
             return res.send({error:true, message:error.message})
           }
+       })
+
+       app.get('/details/:id',async(req,res)=>{
+          try{
+            const id=req.params.id
+            const query={_id: new ObjectId(id)}
+            const result=await blogCollection.findOne(query)
+            return res.send(result)
+          }
+          catch{
+            return res.send({error:true, message:error.message})
+          }
+
        })
 
 
